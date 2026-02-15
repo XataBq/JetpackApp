8359959dc8e49e05fd197dbed7af85333618883b
XataBq <excist97@mail.ru>
2026-02-15 16:02:20 +0300

    authVM login->repo

--- app/src/main/java/com/example/testapp/ui/screens/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/testapp/ui/screens/auth/AuthViewModel.kt
@@ -6,7 +6,6 @@ import com.example.testapp.domain.auth.AuthRepository
 import com.example.testapp.domain.auth.AuthResult
 import com.example.testapp.domain.auth.EmailValidationResult
 import com.example.testapp.domain.auth.EmailValidator
-import com.example.testapp.ui.models.ValidationState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -33,7 +32,6 @@ class AuthViewModel
             )
         val uiState = _uiState.asStateFlow()
         val events = _events.asSharedFlow()
-        val testEmail: String = "1@mail.ru"
 
         fun setFieldFormatErrorFalse() {
             _uiState.update { it.copy(fieldFormatError = false) }
@@ -135,28 +133,27 @@ class AuthViewModel
         fun onLoginClick() {
             val email = uiState.value.email
 
-            val newValidation =
-                if (email == testEmail) {
-                    ValidationState.Success
-                } else {
-                    ValidationState.Error(
-                        "Некорректный Email адрес. Проверьте правильность введенных данных",
-                    )
-                }
-
-            _uiState.update { it.copy(validationState = newValidation) }
-
             viewModelScope.launch {
-                when (newValidation) {
-                    is ValidationState.Error -> {
-                        _uiState.update { it.copy(isLoading = false, fieldFormatError = true) }
-                        _events.tryEmit(AuthEvent.AuthError(newValidation.message))
-                    }
-                    is ValidationState.Success -> {
+                _uiState.update { it.copy(isLoading = true) }
+                when (authRepository.login(email = email)) {
+                    AuthResult.Success -> {
                         _uiState.update { it.copy(isLoading = false, fieldFormatError = false) }
                         _events.emit(AuthEvent.NavigateHome)
                     }
-                    ValidationState.None -> Unit
+
+                    AuthResult.Error.InvalidEmail -> {
+                        _uiState.update { it.copy(isLoading = false, fieldFormatError = true) }
+                        _events.tryEmit(AuthEvent.AuthError("Invalid email"))
+                    }
+
+                    AuthResult.Error.Network -> {
+                        _uiState.update { it.copy(isLoading = false) }
+                        _events.tryEmit(AuthEvent.AuthError("NetworkProblems. Check your Internet connection"))
+                    }
+                    else -> {
+                        _uiState.update { it.copy(isLoading = false) }
+                        _events.tryEmit(AuthEvent.AuthError("Unknown error. Try again, please!)"))
+                    }
                 }
             }
         }
